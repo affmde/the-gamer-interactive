410ec7ec51001f4fa66d94742e0bfaea711738a4
André Miranda <affmde@hotmail.com>
2022-05-02 12:40:46 +0300

    webpak chnaged

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './client/index.js',
   output: {
-    filename: 'index.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'client'),
   },
   module: {
